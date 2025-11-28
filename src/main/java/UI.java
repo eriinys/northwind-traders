a7a78b7fae362791f4544515546f72095545b072
eriinys <eriinys@duck.com>
2025-11-28 15:00:09 -0600

    Created UI class and made HomeScreen method for displaying options. Moved original code in main from exercise 2 under displayProducts method.

---
+++ src/main/java/UI.java
@@ -0,0 +1,77 @@
+import java.util.*;
+import java.sql.*;
+import com.mysql.cj.jdbc.MysqlDataSource;
+
+public class UI {
+    private String user;
+    private String password;
+
+    public UI(String user, String password) {
+        this.user = user;
+        this.password = password;
+    }
+
+    public void HomeScreen(){
+        Scanner scanner = new Scanner(System.in);
+        boolean in = true;
+        while(in){
+            System.out.println("""
+                
+                What do you want to do?
+                    1) Display all products
+                    2) Display all customers
+                    0) Exit
+                Select an option:
+                """);
+            int choice = Integer.parseInt(scanner.nextLine());
+            switch(choice){
+                case 1 -> displayProdcuts();
+                case 2 -> displayCustomers();
+                case 0 -> {
+                    System.out.println("Exiting program...Good bye!");
+                    in = false;
+                }
+            }
+        }
+    }
+
+    public void displayProdcuts(){
+
+        MysqlDataSource dataSource = new MysqlDataSource();
+
+        String url = "jdbc:mysql://localhost:3306/northwind";
+        String user = this.user;
+        String password = this.password;
+
+        String sql = "SELECT ProductID, ProductName, UnitPrice, UnitsInStock FROM products";
+
+        dataSource.setURL(url);
+        dataSource.setUser(user);
+        dataSource.setPassword(password);
+
+        try(Connection conn = dataSource.getConnection();
+            PreparedStatement ps = conn.prepareStatement(sql);
+            ResultSet rs = ps.executeQuery()) {
+
+            System.out.println("""
+                        ID   Name           Price    Stock  
+                        ---- -------------- -------  ------ 
+                        """);
+
+            while (rs.next()) {
+                String ProductID = rs.getString("ProductID");
+                String ProductName = rs.getString("ProductName");
+                String UnitPrice = rs.getString("UnitPrice");
+                String UnitsInStock = rs.getString("UnitsInStock");
+
+                System.out.printf("%-4s %-14s %-8s %1s %n", ProductID, ProductName, UnitPrice, UnitsInStock);
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void displayCustomers(){
+
+    }
+}
--- src/main/java/main.java
+++ src/main/java/main.java
@@ -2,45 +2,15 @@ import java.sql.*;
 import com.mysql.cj.jdbc.MysqlDataSource;
 
 public class main {
-    public static void main(String[] args){
 
+    public static void main(String[] args){
         if (args.length != 2) {
-            System.out.println("Application needs two arguments to run.");
+            System.out.println("Application needs two username and password to run.");
             System.exit(1); //exits program when required arguments are not provided with exit code 1
         }
 
-        MysqlDataSource dataSource = new MysqlDataSource();
-
-        String url = "jdbc:mysql://localhost:3306/northwind";
-        String user = args[0];
-        String password = args[1];
-
-        String sql = "SELECT ProductID, ProductName, UnitPrice, UnitsInStock FROM products";
-
-        dataSource.setURL(url);
-        dataSource.setUser(user);
-        dataSource.setPassword(password);
-
-        try(Connection conn = dataSource.getConnection();
-            PreparedStatement ps = conn.prepareStatement(sql);
-            ResultSet rs = ps.executeQuery()) {
-
-            System.out.println("""
-                        ID   Name           Price    Stock  
-                        ---- -------------- -------  ------ 
-                        """);
-
-            while (rs.next()) {
-                String ProductID = rs.getString("ProductID");
-                String ProductName = rs.getString("ProductName");
-                String UnitPrice = rs.getString("UnitPrice");
-                String UnitsInStock = rs.getString("UnitsInStock");
-
-                System.out.printf("%-4s %-14s %-8s %1s \n", ProductID, ProductName, UnitPrice, UnitsInStock);
-            }
-        } catch (SQLException e) {
-            e.printStackTrace();
-        }
+        UI ui = new UI(args[0], args[1]);
+        ui.HomeScreen();
     }
 }
 
